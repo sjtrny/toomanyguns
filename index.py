7717630d22b3b73465e95d2d6f4641459b4dfb2e
Stephen Tierney <sjtrny@gmail.com>
2019-10-08 11:24:28 +1100

    Removed unused dependencies

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import geopandas as gpd
-import mydcc
 from dash.dependencies import Input, Output, State, ClientsideFunction
 from hurry.filesize import size
 
